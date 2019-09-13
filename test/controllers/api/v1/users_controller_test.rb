6d13e766eb97ee3c94dc029a4e77f7628baf20ee
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2019-09-13 09:45:23 +0200

    Add create action to the users controller

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -10,4 +10,22 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :ok
     assert_equal @user.as_json, response.parsed_body
   end
+
+  test "should create user" do
+    assert_difference("User.count") do
+      post api_v1_users_url, params: { user: { email: "user@example.com", password: "secret" } }
+    end
+
+    assert_response :created
+    assert_equal "user@example.com", response.parsed_body["email"]
+  end
+
+  test "should not create user with taken email" do
+    assert_no_difference("User.count") do
+      post api_v1_users_url, params: { user: { email: @user.email, password: "secret" } }
+    end
+
+    assert_response :unprocessable_entity
+    assert_equal [I18n.t("errors.messages.taken")], response.parsed_body["errors"]["email"]
+  end
 end
