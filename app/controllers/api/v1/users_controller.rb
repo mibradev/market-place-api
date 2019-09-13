a6846bb16de8f76e24485e08a046e9f0547fa25e
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2019-09-13 09:45:45 +0200

    Add update action to the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update]
+
   def show
-    render json: User.find(params[:id])
+    render json: @user
   end
 
   def create
@@ -13,7 +15,19 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: { errors: @user.errors }, status: :unprocessable_entity
+    end
+  end
+
   private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
     def user_params
       params.require(:user).permit(:email, :password, :password_confirmation)
     end
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -20,6 +20,12 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     assert_equal "user@example.com", response.parsed_body["email"]
   end
 
+  test "should update user" do
+    patch api_v1_user_url(@user), params: { user: { email: "user@example.com", password: "secret" } }
+    assert_response :ok
+    assert_equal "user@example.com", response.parsed_body["email"]
+  end
+
   test "should not create user with taken email" do
     assert_no_difference("User.count") do
       post api_v1_users_url, params: { user: { email: @user.email, password: "secret" } }
@@ -27,5 +33,11 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :unprocessable_entity
     assert_equal [I18n.t("errors.messages.taken")], response.parsed_body["errors"]["email"]
+  end
+
+  test "should not update user with invalid params" do
+    patch api_v1_user_url(@user), params: { user: { email: "invalid", password: "secret" } }
+    assert_response :unprocessable_entity
+    assert_equal [I18n.t("errors.messages.invalid")], response.parsed_body["errors"]["email"]
   end
 end
