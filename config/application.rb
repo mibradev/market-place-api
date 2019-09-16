7bcef511916496d177e086c8216b94456f805645
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2019-09-16 14:13:00 +0200

    Configure timezone

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,7 @@ module MarketPlaceApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.time_zone = "Cairo"
   end
 end
