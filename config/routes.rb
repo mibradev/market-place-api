edc0b594de091a34b2b87c7355b4b84260799d36
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2019-12-28 08:36:59 +0200

    Add the show action for order

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :orders, only: :index
+      resources :orders, only: [:index, :show]
       resources :products
       resources :tokens, only: :create
       resources :users
--- test/controllers/api/v1/orders_controller_test.rb
+++ test/controllers/api/v1/orders_controller_test.rb
@@ -15,4 +15,15 @@ class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
     assert_response :ok
     assert_equal @order.user.orders.count, response.parsed_body.length
   end
+
+  test "should not show order if unauthorized" do
+    get api_v1_order_url(@order)
+    assert_response :forbidden
+  end
+
+  test "should show order" do
+    get api_v1_order_url(@order), headers: { "Authorization" => JsonWebToken.encode(user_id: @order.user_id) }
+    assert_response :ok
+    assert_equal @order.products.first.title, response.parsed_body["included"][0]["attributes"]["title"]
+  end
 end
