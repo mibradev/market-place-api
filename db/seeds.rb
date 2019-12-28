9fefdd440d07a1d076f9fcb829a67389b6e20bc7
Muhammad Muhammad Ibrahim <mibradev@gmail.com>
2019-12-28 14:03:10 +0200

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
 1.upto(3) do |user_i|
   user = User.create!(email: "user#{user_i}@example.com", password: "secret")
+  order = user.orders.build
 
   1.upto(3) do |product_i|
-    user.products.create!(title: "Product #{user_i}/#{product_i}")
+    product = user.products.create!(title: "Product #{user_i}/#{product_i}", price: 9.99)
+    order.products << product
   end
+
+  order.save
 end
